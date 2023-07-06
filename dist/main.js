951b5db25db5acd06fe8aeac0fbc25ad7446e1c4
arash <arash.abdous@gmail.com>
2023-07-06 18:53:51 +0330

    Done at lessons 3

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        description: 'A pair of warm, fuzzy socks',
+        image: 'assets/1.png',
+        href: 'https://crm.aasaam.org',
+        onSale: true
+    }
+})
